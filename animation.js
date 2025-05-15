2c423785f3ed0fa1dd989710dab1365be2a9717a
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-05-15 21:16:24 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -4,6 +4,7 @@ function animateText() {
     text.style.transform = "scale(1.2)";
     text.style.color = "#ffcc00";
 
+    
     setTimeout(() => {
         text.style.transform = "scale(1)";
         text.style.color = "#ff69b4";
